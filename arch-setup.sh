c734182aa9444ab4f32cbee912e82258424804c1
CrazyToffy <zqyingzhao@gmail.com>
2022-08-06 21:04:01 +0800

    ctags net-tools htop

--- arch-setup.sh
+++ arch-setup.sh
@@ -15,9 +15,9 @@ show_help() {
 down_files() {
   # 安装必要的软件 
   sudo pacman -Syu --noconfirm \
-              zsh ranger fzf neovim cmake feh lua wget curl llvm\
-              xclip man-db unzip npm highlight tmux clang ninja make\
-              openssh openssl
+              zsh ranger fzf neovim cmake feh lua wget curl llvm \
+              xclip man-db unzip npm highlight tmux clang ninja make \
+              openssh openssl ctags net-tools htop
 
   # zsh-plugins
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/powerlevel10k
